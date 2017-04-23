8822c9707abf6eec096e02b1ed29e0035289f304
Arvind Kala <arvind.kala@bigdecisions.com>
2017-04-24 00:01:07 +0530

    Changes to content rendering file to handle class names for subdirectories in the theme

--- application/views/react/content.php
+++ application/views/react/content.php
@@ -1,12 +1,16 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
-
-$component_class = str_replace('_', '', ucwords($_component_name, '_'));
-
-?>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <script type="text/babel">
 	<?php include __DIR__ . "/react_components/{$_component_name}.jsx"; ?>
 </script>
 
+<?php
+	// Get component class name to render it.
+	$component_class = str_replace('_', '', ucwords($_component_name, '_'));
+
+	if( strpos( $component_class, '/' ) ) {
+		$component_class = ucwords( basename( $component_class ) );	
+	}
+?>
 <script type="text/babel">	
 	ReactDOM.render( < <?php echo $component_class; ?> /> , document.getElementById( '<?php echo $_component_dom_tag_id; ?>' ) );
 </script>
