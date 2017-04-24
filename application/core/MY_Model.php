4f90f2a51d226f1522a9147fb3bc8ad4395a5b6b
Arvind Kala <arvind.kala@bigdecisions.com>
2017-04-24 13:17:04 +0530

    Codeigniter with react with MY_Model addition

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -1,15 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User_Model extends MY_Model {
+class MY_Model extends CI_Model {
 
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->database();
-	}
-
-	protected function validate_login( $email, $password ) {
-
 	}
 }
