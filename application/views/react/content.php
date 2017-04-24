17d5448593d00737dacf98dcce76139e7d9a9bf8
Arvind Kala <arvind.kala@bigdecisions.com>
2017-04-24 13:09:00 +0530

    Codeigniter with react code refactoring

--- application/views/react/content.php
+++ application/views/react/content.php
@@ -1,13 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <script type="text/babel">
-	<?php include __DIR__ . "/modules/{$_module_name}.jsx"; ?>
-</script>
+	<?php
+		$module_class = preg_replace('/[_\/]/', '', ucwords($_module_name, '[_\/]'));
+		include __DIR__ . "/modules/{$_module_name}.jsx"; 
+	?>
 
-<?php
-	// Get component class name to render it.
-	$module_class = preg_replace('/[_\/]/', '', ucwords($_module_name, '[_\/]'));
-?>
-<script type="text/babel">	
 	ReactDOM.render( < <?php echo $module_class; ?> /> , document.getElementById( '<?php echo $_module_dom_tag_id; ?>' ) );
 </script>
