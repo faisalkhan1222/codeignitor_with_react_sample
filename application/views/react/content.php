84911d14916dd055aa19101d672cf7e6aea30ed2
Arvind Kala <kalag.arvind@gmail.com>
2017-04-25 22:54:40 +0530

    Update content.php

--- application/views/react/content.php
+++ application/views/react/content.php
@@ -5,7 +5,7 @@
 		$include_file = "/js/react/components/{$_module_name}.js";
 	?>
 	System.import('<?php echo $include_file; ?>').then(function(module) {
-		
+
 		<?php if( isset($react_vars)) : ?>
 			var props = <?php echo json_encode($react_vars, JSON_FORCE_OBJECT); ?>;
 		<?php endif; ?>
