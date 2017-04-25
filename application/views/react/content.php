76ade86c8f0a43bafe41da6b48a03fd5152e0088
Arvind Kala <arvind.kala@bigdecisions.com>
2017-04-25 18:32:36 +0530

    Modified the code to render the react elements with props

--- application/views/react/content.php
+++ application/views/react/content.php
@@ -5,7 +5,7 @@
 		$include_file = "/js/react/components/{$_module_name}.js";
 	?>
 	System.import('<?php echo $include_file; ?>').then(function(module) {
-		var props = {}
+		var props = '<?php echo json_encode($react_vars, JSON_FORCE_OBJECT); ?>'
 		ReactDOM.render( React.createElement(module.default, props), document.getElementById('content') )
 	})
 </script>
