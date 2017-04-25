bde144d09b9aba2b1e7cd3dce680a369f4d4eee2
Arvind Kala <arvind.kala@bigdecisions.com>
2017-04-25 15:40:37 +0530

    Modified the library for codigniter integration with React JS

--- application/views/react/content.php
+++ application/views/react/content.php
@@ -2,7 +2,6 @@
 
 <script>
 	<?php
-		$module_class = preg_replace('/[_\/]/', '', ucwords($_module_name, '[_\/]'));
 		$include_file = "/js/react/components/{$_module_name}.js";
 	?>
 	System.import('<?php echo $include_file; ?>');
